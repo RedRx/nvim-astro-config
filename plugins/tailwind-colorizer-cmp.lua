9f5974637dca9438dcf613498e841e11dc80da5d
BEARYDEV <teerapat.tech@gmail.com>
2024-03-17 21:58:12 +0700

    update tailwindcss-colorizer-cmp

--- plugins/tailwind-colorizer-cmp.lua
+++ plugins/tailwind-colorizer-cmp.lua
@@ -1,11 +1,8 @@
 return {
   "roobert/tailwindcss-colorizer-cmp.nvim",
+  lazy = false,
   config = function()
-    require("tailwindcss-colorizer-cmp").setup {
-      color_square_width = 2,
-    }
-    require("cmp").config.formatting = {
-      format = require("tailwindcss-colorizer-cmp").formatter,
-    }
+    require("tailwindcss-colorizer-cmp").setup {}
+    require "user.configs.tailwindcss-colorizer-cmp"
   end,
 }
